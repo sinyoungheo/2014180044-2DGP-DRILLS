e88eb2eb45b31d65efc0f49660f33b8ed6d231fd
HeoSinYoung <hechgaya@naver.com>
2019-10-02 18:39:50 +0900

    첫 작업.
    animation 추가.

---
+++ Drill-04/character_runs_esc.py
@@ -0,0 +1,44 @@
+from pico2d import *
+
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+running = True
+x = 0
+frame = 0
+
+
+def handle_events():
+    global running
+    global x
+
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN :
+            if event.key == SDLK_RIGHT:
+                x = x + 10
+            elif event.key == SDLK_LEFT:
+                x = x - 10
+            elif event.key == SDLK_ESCAPE:
+                running = False
+    pass
+
+
+while x < 800 and running:
+    clear_canvas()
+    grass.draw(400, 30)
+    character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+    update_canvas()
+
+    #키가 눌렸는지 확인하는 함수.
+    handle_events()
+
+    frame = (frame + 1) % 8
+    #x += 5
+    delay(0.05)
+
+close_canvas()
