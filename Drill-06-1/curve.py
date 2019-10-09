9a343b3d73e0bad45205d0acf6f65bf5b22dc036
HeoSinYoung <hechgaya@naver.com>
2019-10-09 18:19:06 +0900

    2014180044 허신영 Drill #6-1
    커밋 2회

--- Drill-06-1/curve.py
+++ Drill-06-1/curve.py
@@ -50,6 +50,7 @@ def draw_point(p):
     turtle.dot(5, random.random(), random.random(), random.random())
 
 
+# 3개의 점을 통한 곡선.
 def draw_curve_3_points(p1, p2, p3):
     # fill here
     draw_big_point(p1)
@@ -58,8 +59,8 @@ def draw_curve_3_points(p1, p2, p3):
 
     for i in range(0, 100, 2):
         t = i / 100
-        x = (2 * t**2 - 3 * t + 1) * p1[0] + (-4 * t**2 + 4 * t) * p2[0] + (2 * t**2 - t) * p3[0]
-        y = (2 * t**2 - 3 * t + 1) * p1[1] + (-4 * t**2 + 4 * t) * p2[1] + (2 * t**2 - t) * p3[1]
+        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
 
         draw_point((x, y))
 
@@ -67,6 +68,7 @@ def draw_curve_3_points(p1, p2, p3):
     pass
 
 
+# 4개의 점을 통한 곡선.
 def draw_curve_4_points(p1, p2, p3, p4):
     draw_big_point(p1)
     draw_big_point(p2)
@@ -85,9 +87,9 @@ def draw_curve_4_points(p1, p2, p3, p4):
     for i in range(0, 100, 2):
         t = i / 100
         x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+                -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
+                -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
         draw_point((x, y))
     draw_point(p3)
 
@@ -107,7 +109,7 @@ def draw_curve_4_points_remake(p1, p2, p3, p4):
     draw_big_point(p4)
 
     # draw p1-p2
-    for i in range(0, 50, 2):
+    for i in range(0, 50, 1):
         t = i / 100
         x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
         y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
@@ -118,9 +120,9 @@ def draw_curve_4_points_remake(p1, p2, p3, p4):
     for i in range(0, 100, 2):
         t = i / 100
         x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+                -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
+                -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
         draw_point((x, y))
     draw_point(p3)
 
@@ -135,26 +137,15 @@ def draw_curve_4_points_remake(p1, p2, p3, p4):
     # draw p4-p5
     for i in range(50, 100, 2):
         t = i / 100
-        x = (2 * t ** 2 - 3 * t + 1) * p2[0] + (-4 * t ** 2 + 4 * t) * p3[0] + (2 * t ** 2 - t) * p4[0]
-        y = (2 * t ** 2 - 3 * t + 1) * p2[1] + (-4 * t ** 2 + 4 * t) * p3[1] + (2 * t ** 2 - t) * p4[1]
+        x = (2 * t ** 2 - 3 * t + 1) * p3[0] + (-4 * t ** 2 + 4 * t) * p4[0] + (2 * t ** 2 - t) * p1[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p3[1] + (-4 * t ** 2 + 4 * t) * p4[1] + (2 * t ** 2 - t) * p1[1]
         draw_point((x, y))
     draw_point(p4)
 
 
 prepare_turtle_canvas()
 
-# 3개의 점
-# draw_curve_3_points((-350,-100), (-50, 150), (150, -100))
-
-# draw_curve_3_points((-50, 150), (150, -100), (-350, -100))
-
-# draw_curve_3_points((-350, -100), ( -50, 200), (150, -100))
-# draw_curve_3_points((-50, 200), (150, -100), (350, 300))
-
-
-# 4개의 점
-draw_curve_4_points((-350, -100), ( -50, 200), (150, -100), (350, 300))
-
-# draw_curve_4_points((-300, 200), (400, 350), (300, -300), (-200, -200))
+while True:
+    draw_curve_4_points_remake((-300, 200), (400, 350), (300, -300), (-200, -200))
 
 turtle.done()
