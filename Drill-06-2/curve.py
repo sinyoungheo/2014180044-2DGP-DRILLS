3274f125d0558b5f3caeab4dfa9a317beb9ab979
HeoSinYoung <hechgaya@naver.com>
2019-10-09 19:02:45 +0900

    2014180044 허신영 Drill #06-2 완성
    2014180044 허신영 Drill 06-2 완성
    커밋 횟수 4회
    https://youtu.be/3HOgHm-5ExE

--- Drill-06-2/curve.py
+++ Drill-06-2/curve.py
@@ -154,8 +154,10 @@ def draw_curve_5_points(p1, p2, p3, p4, p5):
     for i in range(0, 100, 2):
         t = i / 100
 
-        x = ((-t**3 + 2 * t**2 - t) * p4[0] + (3 * t**3 - 5 * t**2 + 2) * p1[0] + (-3 * t**3 + 4 * t**2 + t) * p2[0] + (t**3 - t ** 2) * p3[0]) / 2
-        y = ((-t**3 + 2 * t**2 - t) * p4[1] + (3 * t**3 - 5 * t**2 + 2) * p1[1] + (-3 * t**3 + 4 * t**2 + t) * p2[1] + (t**3 - t**2) * p3[1]) / 2
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p2[0] + (t ** 3 - t ** 2) * p3[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p2[1] + (t ** 3 - t ** 2) * p3[1]) / 2
 
         draw_point((x, y))
 
@@ -165,8 +167,10 @@ def draw_curve_5_points(p1, p2, p3, p4, p5):
     for i in range(0, 100, 2):
         t = i / 100
 
-        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
 
         draw_point((x, y))
 
@@ -176,8 +180,10 @@ def draw_curve_5_points(p1, p2, p3, p4, p5):
     for i in range(0, 100, 2):
         t = i / 100
 
-        x = ((-t**3 + 2 * t**2 - t) * p2[0] + (3 * t**3 - 5 * t**2 + 2) * p3[0] + (-3 * t**3 + 4 * t**2 + t) * p4[0] + (t**3 - t**2) * p5[0]) / 2
-        y = ((-t**3 + 2 * t**2 - t) * p2[1] + (3 * t**3 - 5 * t**2 + 2) * p3[1] + (-3 * t**3 + 4 * t**2 + t) * p4[1] + (t**3 - t**2) * p5[1]) / 2
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p2[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p4[0] + (t ** 3 - t ** 2) * p5[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p2[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p5[1]) / 2
 
         draw_point((x, y))
 
@@ -187,19 +193,77 @@ def draw_curve_5_points(p1, p2, p3, p4, p5):
     for i in range(0, 100, 2):
         t = i / 100
 
-        x = ((-t**3 + 2 * t**2 - t) * p3[0] + (3 * t**3 - 5 * t**2 + 2) * p4[0] + (-3 * t**3 + 4 * t**2 + t) * p5[0] + (t**3 - t**2) * p2[0]) / 2
-        y = ((-t**3 + 2 * t**2 - t) * p3[1] + (3 * t**3 - 5 * t**2 + 2) * p4[1] + (-3 * t**3 + 4 * t**2 + t) * p5[1] + (t**3 - t**2) * p2[1]) / 2
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p3[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p5[0] + (t ** 3 - t ** 2) * p2[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p5[1] + (t ** 3 - t ** 2) * p2[1]) / 2
 
         draw_point((x, y))
 
     draw_point(p1)
 
 
+def draw_start_end(p1, p2, p3, near, far):
+    for i in range(near, far, 2):
+        t = i / 100
+
+        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
+
+        draw_point((x, y))
+
+
+def draw_middle(p1, p2, p3, p4, near, far):
+    for i in range(near, far, 2):
+        t = i / 100
+
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
+
+        draw_point((x, y))
+
+
+def draw_curve_11_points(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
+    global way
+    # draw p1-p2
+    draw_middle(p10, p1, p2, p3, 0, 100)
+
+    # draw p2-p3
+    draw_middle(p1, p2, p3, p4, 0, 100)
+
+    # draw p3-p4
+    draw_middle(p2, p3, p4, p5, 0, 100)
+
+    # draw p4-p5
+    draw_middle(p3, p4, p5, p6, 0, 100)
+
+    # draw p5-p6
+    draw_middle(p4, p5, p6, p7, 0, 100)
+
+    # draw p6-p7
+    draw_middle(p5, p6, p7, p8, 0, 100)
+
+    # draw p7-p8
+    draw_middle(p6, p7, p8, p9, 0, 100)
+
+    # draw p8-p9
+    draw_middle(p7, p8, p9, p10, 0, 100)
+
+    # draw p9-p10
+    draw_middle(p8, p9, p10, p1, 0, 100)
+
+    # draw p10-p11
+    draw_middle(p9, p10, p1, p2, 0, 100)
+
+
 prepare_turtle_canvas()
 
-points = [(-300, 200), (400, 350), (300, -300), (-200, -200)]
+# 10개의 랜덤한 점 지정.
+point = [(random.randint(0, 300), random.randint(-300, 0)) for i in range(10)]
 
 while True:
-    draw_curve_5_points(points[0], points[1], points[2], points[3], points[0])
+    draw_curve_11_points(point[0], point[1], point[2], point[3], point[4], point[5], point[6], point[7], point[8], point[9])
 
 turtle.done()
--- Drill-06-2/move_character.py
+++ Drill-06-2/move_character.py
@@ -1,104 +1,129 @@
 from pico2d import *
-import math
+import random
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
-
-# 각도 계산
-def Cal_Degree(ax, bx, ay, by):
-    w = ax - bx
-    h = ay - by
-    d = math.sqrt(pow(w, 2) + pow(h, 2))
-
-    angle = math.acos(w / d)
-
-    if by > ay:
-        angle = angle * -1
-
-    return angle * 180.0 / math.pi
-
-
-# 마우스 이벤트
-def handle_events():
-    # fill here
-    global running
-    global x, y
-    global tempx, tempy
-    global bIsMove
-
-    events = get_events()
-
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, KPU_HEIGHT - 1 - event.y
-
-        elif event.type == SDL_MOUSEBUTTONDOWN:
-            if event.button == SDL_BUTTON_LEFT:
-                tempx, tempy = event.x, KPU_HEIGHT - 1 - event.y
-                bIsMove = True
-
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
-    pass
-
-
-# fill here
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
-mouse = load_image('hand_arrow.png')
+
+# 10개의 랜덤한 점 지정.
+point = [(random.randint(100, 600), random.randint(100, 600)) for i in range(10)]
+
+global way
 
 running = True
 
-x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+# 키보드 입력.
+def handle_events():
+    # fill here
+    global running  # 달리는 것
 
-movex, movey = KPU_WIDTH // 2, KPU_HEIGHT // 2
-tempx, tempy = KPU_WIDTH // 2, KPU_HEIGHT // 2  # 마우스 값
+    events = get_events()
+    for event in events:
 
-frame = 0
+        if event.type == SDL_QUIT:
+            running = False
 
-hide_cursor()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
 
-myAngle = 0.0
 
-bIsMove = False
+def move_point(p):
+    global frame
 
-dirx = tempx
-dir = 100
-
-while running:
     clear_canvas()
 
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    mouse.draw(x, y)
-
-    character.clip_draw(frame * 100, dir, 100, 100, movex, movey)
+    character.clip_draw(frame * 100, 100 * way, 100, 100, p[0], p[1])
 
     update_canvas()
 
     frame = (frame + 1) % 8
 
-    delay(0.03)
+    delay(0.05)
+
     handle_events()
 
-    dirx = movex
 
-    if dirx < tempx:
-        dir = 100
-    elif dirx > tempx:
-        dir = 0
+def draw_start_end(p1, p2, p3, near, far):
+    for i in range(near, far, 2):
+        t = i / 100
 
-    if (tempx - 3 <= movex <= tempx + 3 and
-            tempy - 3 <= movey <= tempy + 3 and bIsMove == True):
-        bIsMove = False
+        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
 
-    if bIsMove == True:
-        myAngle = Cal_Degree(movex, tempx, movey, tempy)
-        movex -= math.cos(myAngle * math.pi / 180.0) * 5.0
-        movey -= math.sin(myAngle * math.pi / 180.0) * 5.0
+        move_point((x, y))
+
+
+def draw_middle(p1, p2, p3, p4, near, far):
+    direct_character(p2, p3)
+
+    for i in range(near, far, 2):
+        t = i / 100
+
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (-3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (-3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
+
+        move_point((x, y))
+
+
+def direct_character(p1, p2):
+    global way
+
+    if p1[0] < p2[0]:
+        way = 1
+    elif p1[0] > p2[0]:
+        way = 0
+
+
+def draw_curve_11_points(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
+    global way
+    # draw p1-p2
+    draw_middle(p10, p1, p2, p3, 0, 100)
+
+    # draw p2-p3
+    draw_middle(p1, p2, p3, p4, 0, 100)
+
+    # draw p3-p4
+    draw_middle(p2, p3, p4, p5, 0, 100)
+
+    # draw p4-p5
+    draw_middle(p3, p4, p5, p6, 0, 100)
+
+    # draw p5-p6
+    draw_middle(p4, p5, p6, p7, 0, 100)
+
+    # draw p6-p7
+    draw_middle(p5, p6, p7, p8, 0, 100)
+
+    # draw p7-p8
+    draw_middle(p6, p7, p8, p9, 0, 100)
+
+    # draw p8-p9
+    draw_middle(p7, p8, p9, p10, 0, 100)
+
+    # draw p9-p10
+    draw_middle(p8, p9, p10, p1, 0, 100)
+
+    # draw p10-p11
+    draw_middle(p9, p10, p1, p2, 0, 100)
+
+
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
+
+running = True
+
+
+frame = 0
+way = 1
+
+while running:
+    draw_curve_11_points(point[0], point[1], point[2], point[3], point[4], point[5], point[6], point[7], point[8], point[9])
+    handle_events()
 
 close_canvas()
