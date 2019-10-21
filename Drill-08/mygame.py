dd17155ea000a355ede5b66e49c1bc216e0c5289
HeoSinYoung <hechgaya@naver.com>
2019-10-21 18:44:59 +0900

    2014180044 허신영 Drill # 08 준비
    프로젝트 준비.
    커밋 1회

---
+++ Drill-08/mygame.py
@@ -0,0 +1,13 @@
+import game_framework
+
+# fill here
+import  game_framework
+import  pico2d
+
+import  start_state
+
+pico2d.open_canvas()
+
+game_framework.run(start_state)
+
+pico2d.clear_canvas()
