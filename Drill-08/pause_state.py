89b7944133688181743b85ee4af41ea7b4d429e3
HeoSinYoung <hechgaya@naver.com>
2019-10-21 19:26:33 +0900

    2014180044 허신영 Drill # 08-2 완성
    커밋횟수 3회
    2014180044 허신영 Drill08-2 완성
    https://youtu.be/yAR_b91zSKQ

--- Drill-08/pause_state.py
+++ Drill-08/pause_state.py
@@ -5,9 +5,25 @@ import main_state
 image = None
 
 
+class Pause:
+    def __init__(self):
+        self.image = load_image('pause.png')
+        self.switch = 0
+
+    def draw(self):
+        if self.switch == 1:
+            self.image.clip_draw(230, 230, 430, 430, 400, 300)
+
+    def update(self):
+        self.switch = (self.switch + 1) % 2
+        delay(0.05)
+
+
 def enter():
     global image
-    image = load_image('pause.png')
+
+    # 이미지를 클래스로 생성.
+    image = Pause()
     pass
 
 
@@ -23,7 +39,7 @@ def handle_events():
         if event.type == SDL_QUIT:
             game_framework.quit()
         else:
-            # p 버튼을 누르면 game_framework에서 pop_state
+            # p 버튼을 누르면 game_framework 에서 pop_state
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
                 game_framework.pop_state()
 
@@ -31,13 +47,20 @@ def handle_events():
 def draw():
     clear_canvas()
 
-    image.clip_draw(230, 230, 430, 430, 400, 300)
+    # main state에 있는 boy와 grass 랜더링
+    main_state.boy.draw()
+    main_state.grass.draw()
+
+    # pause Logo 랜더링
+    image.draw()
 
     update_canvas()
+
     pass
 
 
 def update():
+    image.update()
     pass
 
 
